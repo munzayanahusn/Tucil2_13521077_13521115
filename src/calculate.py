83d76d94e7aa7579fbde1548a3c5521b550aeb61
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-02-25 11:37:08 +0700

    add proses divide combine

--- src/calculate.py
+++ src/calculate.py
@@ -1,7 +1,23 @@
 import math
 import numpy
 
-# Perhitungan jarak
+'''
+Algoritma Divide Conquer
+Divide  : Bagi himpunan titik menjadi 2 bagian seimbang (jumlah titik tiap bagian sama)
+Conquer : Solve jika jumlah titik suatu bagian 2 atau 3, cari distance minimum
+Combine : Tentukan nilai jarak antar point minimum, terdapat 3 kasus:
+          (a) Pasangan titik terdepat terdapat pada bagian kanan (rightPoint)
+          (b) Pasangan titik terdepat terdapat pada bagian kiri (leftPoint)
+          (c) Pasangan titik terdepat terdapat pada pasangan titik yang dipisahkan garis maya pembagi (midPoint)
+'''
+
+
+# Inisiasi variabel global minimum distance dan point
+minDistance = 0
+minPoint1 = 0
+minPoint2 = 0
+
+# Function menghitung euclidean distance
 
 
 def euclidDistance(point):
@@ -14,10 +30,45 @@ def euclidDistance(point):
 
     return math.sqrt(dis)
 
-# Proses Divide
+# Function menentukan minimum distance kasus 3
+# def midPointDistance()
 
 
 def findPair(point):
-    # Proses Divide
-    if (len(point) <= 2):
-        dis = euclidDistance(point[0], point[1])
+    # Proses Conquer / solve
+    if (len(point) == 2):
+        minDistance = euclidDistance(point)
+        minPoint1 = point[0]
+        minPoint2 = point[1]
+    elif (len(point) == 3):
+        # Compare P0 dan P1
+        tempPoint = [point[0], point[1]]
+        minDistance = euclidDistance(tempPoint)
+        minPoint1 = tempPoint[0]
+        minPoint2 = tempPoint[1]
+
+        # Compare P0 dan P2
+        tempPoint = [point[0], point[2]]
+        tempDistance = euclidDistance(tempPoint)
+        if (minDistance > tempDistance):
+            minDistance = tempDistance
+            minPoint1 = tempPoint[0]
+            minPoint2 = tempPoint[1]
+
+        # Compare P1 dan P2
+        tempPoint = [point[1], point[2]]
+        tempDistance = euclidDistance(tempPoint)
+        if (minDistance > tempDistance):
+            minDistance = tempDistance
+            minPoint1 = tempPoint[0]
+            minPoint2 = tempPoint[1]
+    else:
+        # Proses Divide
+        leftPoint = point[:(len(point)//2)]
+        rightPoint = point[((len(point)//2) + 1):]
+        leftDistance = findPair(leftPoint)
+        rightDistance = findPair(rightPoint)
+
+        # Proses Combine
+        minDistance = min(leftDistance, rightDistance)
+        # minDistance = min(minDistance, midPointDistance)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 import math
 import makePoint
+import calculate
 
-n = int(input())
-res = makePoint.randomPoint(n)
-print(res)
+# Mengurutkan point berdasarkan nilai sumbu x
+# Perlu ga
+
+n = int(input())                    # Jumlah titik atau point
+point = makePoint.randomPoint(n)    # Menghasilkan array of point
+point.sort()
+# calculate.findPair(point)
+print(point)
